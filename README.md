c172e32c20c82b7a87a224b11684895c709582c8
刘 冲 <Y-Y-Liu@hotmail.com>
2022-04-06 22:42:59 +0800

    提交完整代码

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # MessageCenter
+
+简单的C#进程内消息转发扩展
+
+## 使用
+
+```C#
+Foo foo = new Foo();
+Bar bar = new Bar();
+//注意，要先注册消息订阅
+bar.Start();
+foo.Start();
+
+
+
+public class Bar
+{
+    public void Start()
+    {
+        MessageCenter.Subscribe<Foo, string>(this, "测试通道", Handle);
+    }
+
+    private void Handle(Foo sender, string data)
+    {
+        Console.WriteLine(data);
+    }
+}
+
+public class Foo
+{
+    public void Start()
+    {
+        MessageCenter.Send(this, "测试通道", "这是一条测试信息");
+    }
+}
+```
---
+++ src/MessageCenter/MessageCenter/MessageCenter.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+public static class MessageCenter
+{
+    class Subscription
+    {
+        public Subscription(object source, MethodInfo methodInfo, object target)
+        {
+            Source = source;
+            MethodInfo = methodInfo;
+            Target = target;
+        }
+        public object Source { get; set; }
+        public MethodInfo MethodInfo { get; set; }
+        public object Target { get; set; }
+        public void Invoke(object sender, object data)
+        {
+            if (sender == null || sender == Source) return;
+            if (MethodInfo.IsStatic)
+            {
+                MethodInfo.Invoke(null, MethodInfo.GetParameters().Length == 1 ? new[] { sender } : new[] { sender, data });
+                return;
+            }
+            MethodInfo?.Invoke(Target, MethodInfo.GetParameters().Length == 2 ? new[] { sender, data } : new[] { sender });
+        }
+    }
+    private static Dictionary<string, List<Subscription>> subs = new Dictionary<string, List<Subscription>>();
+
+    public static void InnerSend(object sender, string name, object data)
+    {
+        if (string.IsNullOrEmpty(name))
+            throw new NullReferenceException();
+        if (subs.TryGetValue(name, out var subscriptions))
+        {
+            foreach (var subscription in subscriptions)
+            {
+                subscription.Invoke(sender, data);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 发布消息
+    /// </summary>
+    /// <param name="sender">消息发送者</param>
+    /// <param name="name">管道名</param>
+    /// <param name="data">消息</param>
+    public static void Send(object sender, string name, object data)
+    {
+        InnerSend(sender, name, data);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="name"></param>
+    public static void Send(object sender, string name)
+    {
+        InnerSend(sender, name, null);
+    }
+
+    private static void InnerSubscribe(object subscriber, string name, MethodInfo methodInfo, object target)
+    {
+        if (subscriber == null)
+            throw new NullReferenceException();
+        if (string.IsNullOrEmpty(name))
+            throw new NullReferenceException();
+        var sub = new Subscription(subscriber, methodInfo, target);
+        if (subs.ContainsKey(name))
+        {
+            subs[name].Add(sub);
+            return;
+        }
+        subs.Add(name, new List<Subscription>() { sub });
+    }
+
+    /// <summary>
+    /// 订阅消息
+    /// </summary>
+    /// <typeparam name="TSender">发送者类型</typeparam>
+    /// <param name="subscriber">订阅者</param>
+    /// <param name="name">管道名</param>
+    /// <param name="action">订阅回调</param>
+    public static void Subscribe<TSender>(object subscriber, string name, Action<TSender> action)
+    {
+        InnerSubscribe(subscriber, name, action?.Method, action?.Target);
+    }
+
+    /// <summary>
+    /// 订阅消息
+    /// </summary>
+    /// <typeparam name="TSender">发送者类型</typeparam>
+    /// <typeparam name="TData">消息类型</typeparam>
+    /// <param name="subscriber">订阅者</param>
+    /// <param name="name">管道名</param>
+    /// <param name="action">订阅回调</param>
+    public static void Subscribe<TSender, TData>(object subscriber, string name, Action<TSender, TData> action)
+    {
+        InnerSubscribe(subscriber, name, action?.Method, action?.Target);
+    }
+
+    /// <summary>
+    /// 取消订阅
+    /// </summary>
+    /// <param name="subscriber">订阅者</param>
+    /// <param name="name">管道名</param>
+    public static void UnSubscribe(object subscriber, string name)
+    {
+        if (subs.TryGetValue(name, out var act))
+        {
+            if (act == null && act.Count == 0)
+            {
+                subs.Remove(name);
+                return;
+            }
+            subs[name].RemoveAll(x => x.Source == subscriber);
+            if (!subs[name].Any())
+                subs.Remove(name);
+            return;
+        }
+    }
+}
+
+
