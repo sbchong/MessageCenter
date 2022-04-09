e279140abd506ed7d39c482eb302c3148a53e836
刘 冲 <Y-Y-Liu@hotmail.com>
2022-04-10 01:31:54 +0800

    测试

---
+++ src/MessageCenter/MessageCenter.Test/UnitTest.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MessageCenter.Test
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void Test()
+        {
+            const string CHANNEL = "测试通道";
+
+            string result = string.Empty;
+            MessageCenter.Subscribe<UnitTest, string>(this, CHANNEL, (s, e) => result = e);
+
+            string data = "测试数据";
+            MessageCenter.Send(this, CHANNEL, data);
+
+            Assert.IsTrue(result != string.Empty);
+            Assert.IsTrue(result == data);
+        }
+
+    }
+}
